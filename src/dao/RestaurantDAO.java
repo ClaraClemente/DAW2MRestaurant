f77e3a5f395d045b65a9b2352ce0c81ad0684da3
mfontana <mfontana@jupiter>
2017-12-05 09:40:42 +0100

    Implementada función insertar cocinero y testeada.

--- src/dao/RestaurantDAO.java
+++ src/dao/RestaurantDAO.java
@@ -5,7 +5,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import modelo.Cocinero;
 
 /**
  *
@@ -13,6 +15,24 @@ import java.sql.SQLException;
  */
 public class RestaurantDAO {
     private Connection conexion;
+    
+    public void insertarCocinero(Cocinero c) throws SQLException {
+        // Definimos la consulta
+        String insert = "insert into cocinero values (?, ?, ?, ?, ?, ?)";
+        // Necesitamos preparar la consulta parametrizada
+        PreparedStatement ps = conexion.prepareStatement(insert);
+        // Le damos valor a los interrogantes
+        ps.setString(1, c.getNombre());
+        ps.setString(2, c.getTelefono());
+        ps.setString(3, c.getSexo());
+        ps.setInt(4, c.getEdad());
+        ps.setInt(5, c.getExperiencia());
+        ps.setString(6, c.getEspecialidad());
+        // Ejecutamos la consulta
+        ps.executeUpdate();
+        // cerramos recursos
+        ps.close();
+    }
     
     public void conectar() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/restaurant";
--- src/daw2mrestaurant/DAW2MRestaurant.java
+++ src/daw2mrestaurant/DAW2MRestaurant.java
@@ -7,6 +7,7 @@ import dao.RestaurantDAO;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import modelo.Cocinero;
 
 /**
  *
@@ -24,14 +25,29 @@ public class DAW2MRestaurant {
         try {
             restaurantDAO.conectar();
             System.out.println("Establecida la conexión.");
-            
+            Cocinero c1 = new Cocinero("Cocinillas Pérez", "12345555", "Hombre", 30, 10, "Postres");
+            System.out.println("Testeando insert cocinero " + c1.getNombre());
+            try {
+                restaurantDAO.insertarCocinero(c1);
+                System.out.println("Cocinero dado de alta");
+            } catch (SQLException ex) {
+                System.out.println("Error al insertar: " + ex.getMessage());
+            }
+            Cocinero c2 = new Cocinero("Maria Castañera", "33333333", "Mujer", 23, 0, "Entrantes");
+            System.out.println("Testeando insert cocinero " + c2.getNombre());
+            try {
+                restaurantDAO.insertarCocinero(c2);
+                System.out.println("Cocinero dado de alta");
+            } catch (SQLException ex) {
+                System.out.println("Error al insertar " + ex.getMessage());
+            }
             System.out.println("Cerrando conexión con la base de datos");
             restaurantDAO.desconectar();
             System.out.println("Conexión cerrada.");
         } catch (SQLException ex) {
-            System.out.println("Error al conectar / desconectar: "+ex.getMessage());
+            System.out.println("Error al conectar / desconectar: " + ex.getMessage());
         }
-        
+
     }
-    
+
 }
